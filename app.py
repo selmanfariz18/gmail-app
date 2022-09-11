871e036422431cb3f9782c36878f7c443832bd39
selmanfariz18 <salmanulfarizyka@gmail.com>
2022-09-11 14:08:05 +0530

    Project compleated

--- app.py
+++ app.py
@@ -3,9 +3,38 @@ import smtplib
 
 i=0
 
+def send_message(email_body,address):
+    global a
+    address_info = address
+    
+    email_body_info = email_body
+    
+    server.sendmail(email,address_info,email_body_info)
+    
+    print("Message sent")
+
 def s_mail():
     global a
 
+    address_field = Label(text="Recipient Address :")
+    email_body_field = Label(text="Message :")
+
+    address_field.place(x=15,y=290)
+    email_body_field.place(x=15,y=360)
+
+    address = StringVar()
+    email_body = StringVar()
+
+
+    address_entry = Entry(textvariable=address,width="30")
+    email_body_entry = Entry(textvariable=email_body,width="30")
+
+    address_entry.place(x=15,y=320)
+    email_body_entry.place(x=15,y=400)
+
+    button = Button(app,text="Send Message",command=lambda: send_message(email_body.get(),address.get()),width="15",height="1",bg="grey")
+
+    button.place(x=15,y=440)
 
 def log():
     global a
@@ -21,20 +50,34 @@ def log():
         a.place(x=30,y=230)
 
 def login():
+    global server
+    global email
     email=mail.get()
     password=pas.get()
     global i
+    sender_email = email 
+    
+    sender_password = password
+    
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    
+    server.starttls()
+    
+    server.login(sender_email,sender_password)
+    
+    print("Login successful")
     i=1
     log()
 
 app=Tk()
 app.geometry("500x500")
 app.title("Login")
-heading=Label(text="Login")
+heading=Label(text="Gmail App")
 heading.pack()
 
 log_mail = Label(text="Email:")
-passwd = Label(text="password:")
+passwd = Label(text="password:\n(Use app password generated from google security)")
+
  
 log_mail.place(x=30,y=20)
 passwd.place(x=30,y=90)
@@ -50,6 +93,9 @@ pass_entry.place(x=30,y=130)
 
 button = Button(app,text="login",command=login,width="15",height="1",bg="grey")
 button.place(x=30,y=170)
-print(i)
+
+
+d=Label(text="Developed by selmanul farizy")
+d.place(x=40,y=480)
 
 mainloop()
